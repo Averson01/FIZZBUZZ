a8d37e17a1bffe1780aa45e60c6368a72f01c56d
Averson01 <167694823+Averson01@users.noreply.github.com>
2024-12-27 19:07:14 +0000

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+ for(i=1; i<=100;i++){
+
+    if(i%3==0){
+        console.log("fiz")
+    } if(i%5==0){
+        console.log("buzz")
+    } if(i%3==0 & i%5==0){
+        console.log("FizzBuzz")
+    }if(i%3!=0 & i%5!=0){
+        console.log(i)
+    }
+
+ }
